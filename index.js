954303469bc39afaf3692ca5d7b719cc8205ae18
rajubhuiyan <rajubhuiyaann@gmail.com>
2021-11-08 12:23:38 +0600

    done

--- index.js
+++ index.js
@@ -16,17 +16,25 @@ const uri = "mongodb+srv://powerx:powerx@cluster0.uueml.mongodb.net/powerX?retry
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db("powerX").collection("CategoryData");
+    const shippingAndPayment = client.db("powerX").collection("ShippingAndPayment");
     app.get('/getCategoryItem', (req, res) => {
         collection.find({})
-        .toArray((err, document) => {
-            res.send(document)
-        })
+            .toArray((err, document) => {
+                res.send(document)
+            })
     })
 
     app.get('/category/:categoryName', (req, res) => {
-        collection.find({category: req.params.categoryName})
+        collection.find({ category: req.params.categoryName })
+            .toArray((err, document) => {
+                res.send(document[0])
+            })
+    })
+    app.post('/sendPayment', (req, res) => {
+        const payData = req.body;
+        shippingAndPayment.insertOne({...payData})
         .toArray((err, document) => {
-            res.send(document[0])
+            res.send(document);
         })
     })
 });
